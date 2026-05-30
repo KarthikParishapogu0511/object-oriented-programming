1257f3b159e242b381b4aae56ae5d76b2f09c5ac
KarthikParishapogu0511 <karthikparishapogu0511@gmail.com>
2026-05-30 11:27:58 +0530

    Customer Array and Methods

--- src/com/java/oop/arrays/Customer.java
+++ src/com/java/oop/arrays/Customer.java
@@ -4,15 +4,17 @@ public class Customer {
     int id;
     String name;
     int age;
+    char gender;
     long mobileNumber;
     String email;
     String address;
     boolean isAvailable;
 
-    public Customer(int id, String name, int age, long mobileNumber, String email, String address, boolean isAvailable) {
+    public Customer(int id, String name, int age,char gender, long mobileNumber, String email, String address, boolean isAvailable) {
         this.id = id;
         this.name = name;
         this.age = age;
+        this.gender = gender;
         this.mobileNumber = mobileNumber;
         this.email = email;
         this.address = address;
@@ -20,9 +22,11 @@ public class Customer {
     }
 
     void printCustomerDetails(){
+        System.out.println("------- Customer Details ----------");
         System.out.println("Customer Id : " + id);
         System.out.println("Customer Name : " + name);
         System.out.println("Customer Age : " + age);
+        System.out.println("Customer Gender : " + gender);
         System.out.println("Customer mobileNumber : "+ mobileNumber);
         System.out.println("Customer email : " + email);
         System.out.println("Customer address : " + address);
--- src/com/java/oop/arrays/CustomersArray.java
+++ src/com/java/oop/arrays/CustomersArray.java
@@ -3,5 +3,105 @@ package com.java.oop.arrays;
 public class CustomersArray {
     public static void main(String[] args) {
         Customer[] customers = new Customer[5];
+
+        customers[0] = new  Customer(1,"Karthik1",22,'M',87976535321L,"karthik765@gmail.com","CB Hostel",true);
+        customers[1] = new  Customer(3,"Karthik3",23,'F',8796079521L,"karthik715@gmail.com","JB Hostel",false);
+        customers[2] = new  Customer(5,"Karthik5",25,'M',6347653321L,"karthik735@gmail.com","AB Hostel",true);
+        customers[3] = new  Customer(2,"Karthik2",27,'F',8826535321L,"karthik865@gmail.com","OB Hostel",false);
+        customers[4] = new  Customer(4,"Karthik4",29,'M',7797535321L,"karthik764@gmail.com","KB Hostel",true);
+
+        for(Customer customer : customers){
+            customer.printCustomerDetails();
+        }
+
+        // sort all the customers by name
+        //find the customer with highest age
+//        Customer highestAgeCustomer = customers[0];
+//
+//        for(Customer customer : customers){
+//            if(customer.age > highestAgeCustomer.age){
+//                highestAgeCustomer = customer;
+//            }
+//        }
+//        highestAgeCustomer.printCustomerDetails();
+
+
+        //no of male customers
+
+//        int maleCount = 0;
+//        for(Customer customer : customers){
+//            if(customer.gender == 'M'){
+//                maleCount++;
+//            }
+//        }
+//        System.out.println("Male Count : "+ maleCount);
+        System.out.println("Male Customers Count : " + getMaleCustomerCount(customers));
     }
+    public static int getMaleCustomerCount(Customer[] customers){
+        int maleCount = 0;
+        for(Customer customer : customers){
+            if(customer.gender == 'M'){
+                maleCount++;
+            }
+        }
+        return maleCount;
+    }
+
+    public static int getFemaleCustomerCount(Customer[] customers){
+        int femaleCount = 0;
+        for(Customer customer : customers){
+            if(customer.gender == 'F'){
+                femaleCount++;
+            }
+        }
+        return femaleCount;
+    }
+
+    public static Customer getCustomerById(Customer[] customers,int id){
+        for(Customer customer : customers){
+            if(customer.id == id){
+                customer.printCustomerDetails();
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    public static Customer getCustomerByName(Customer[] customers,String name){
+        for(Customer customer : customers){
+            if(customer.name.equals(name)){
+                customer.printCustomerDetails();
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    public static Customer getHighestAgedCustomer(Customer[] customers){
+        Customer highestAgeCustomer = customers[0];
+
+        for(Customer customer : customers){
+            if(customer.age > highestAgeCustomer.age){
+                highestAgeCustomer = customer;
+            }
+        }
+//        highestAgeCustomer.printCustomerDetails();
+        return highestAgeCustomer;
+    }
+
+    public static Customer[] sortCustomersByName(Customer[] customers){
+        return null;
+    }
+
+    public static int customersWithAgeGreaterThan(Customer[] customers,int age){
+        return 0;
+    }
+
+    public  static int customersWithAgeBetween(Customer[] customers,int minAge,int maxAge){
+        return 0;
+    }
+
+
+
+
 }
